970d99ed0842ed81c6f81d18959d3691e1f869d9
DouglasMV <32845487+DouglasMV@users.noreply.github.com>
2019-05-30 17:56:25 -0300

    translated test

--- 1-js/05-data-types/02-number/3-repeat-until-number/_js.view/test.js
+++ 1-js/05-data-types/02-number/3-repeat-until-number/_js.view/test.js
@@ -8,29 +8,29 @@ afterEach(function() {
 
 describe("readNumber", function() {
 
-  it("if a number, returns it", function() {
+  it("se for um número, retorne-o", function() {
     prompt.returns("123");
     assert.strictEqual(readNumber(), 123);
   });
 
-  it("if 0, returns it", function() {
+  it("se 0, retorne-o", function() {
     prompt.returns("0");
     assert.strictEqual(readNumber(), 0);
   });
 
-  it("continues the loop unti meets a number", function() {
-    prompt.onCall(0).returns("not a number");
-    prompt.onCall(1).returns("not a number again");
+  it("continue o loop até encontrar um número", function() {
+    prompt.onCall(0).returns("não é um número");
+    prompt.onCall(1).returns("não é um número denovo");
     prompt.onCall(2).returns("1");
     assert.strictEqual(readNumber(), 1);
   });
 
-  it("if an empty line, returns null", function() {
+  it("se for uma linha vazia, retorne null", function() {
     prompt.returns("");
     assert.isNull(readNumber());
   });
 
-  it("if cancel, returns null", function() {
+  it("se cancelar, retorne null", function() {
     prompt.returns(null);
     assert.isNull(readNumber());
   });
