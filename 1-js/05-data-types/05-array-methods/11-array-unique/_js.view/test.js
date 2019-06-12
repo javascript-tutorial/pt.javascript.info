29d25e3d60fc3dbb5dc36d7e8827c2b86ae8731e
DouglasMV <32845487+DouglasMV@users.noreply.github.com>
2019-06-12 17:32:25 -0300

    Translated test.js

--- 1-js/05-data-types/05-array-methods/11-array-unique/_js.view/test.js
+++ 1-js/05-data-types/05-array-methods/11-array-unique/_js.view/test.js
@@ -1,5 +1,5 @@
 describe("unique", function() {
-  it("removes non-unique elements", function() {
+  it("remove elementos não únicos", function() {
     let strings = ["Hare", "Krishna", "Hare", "Krishna",
       "Krishna", "Krishna", "Hare", "Hare", ":-O"
     ];
@@ -7,7 +7,7 @@ describe("unique", function() {
     assert.deepEqual(unique(strings), ["Hare", "Krishna", ":-O"]);
   });
 
-  it("does not change the source array", function() {
+  it("não altera o array de origem", function() {
     let strings = ["Krishna", "Krishna", "Hare", "Hare"];
     unique(strings);
     assert.deepEqual(strings, ["Krishna", "Krishna", "Hare", "Hare"]);
