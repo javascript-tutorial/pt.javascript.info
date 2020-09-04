8aec4a93fe55da4c9bdc11d671ec0a25d4586578
Osvaldo Dias dos Santos <vadodsantos@yahoo.com>
2020-09-04 23:32:09 +0100

    Update test.js

--- 1-js/04-object-basics/01-object/8-multiply-numeric/_js.view/test.js
+++ 1-js/04-object-basics/01-object/8-multiply-numeric/_js.view/test.js
@@ -1,17 +1,17 @@
 describe("multiplyNumeric", function() {
-  it("multiplies all numeric properties by 2", function() {
+  it("multiplica todas as propriedades numéricas por 2", function() {
     let menu = {
       width: 200,
       height: 300,
-      title: "My menu"
+      title: "O meu menu"
     };
     let result = multiplyNumeric(menu);
     assert.equal(menu.width, 400);
     assert.equal(menu.height, 600);
-    assert.equal(menu.title, "My menu");
+    assert.equal(menu.title, "O meu menu");
   });
 
-  it("returns nothing", function() {
+  it("não retorna nada", function() {
     assert.isUndefined( multiplyNumeric({}) );
   });
 
