6b0dfb0dbf092e62599006e17c4fccb0ee606dea
Osvaldo Dias dos Santos <vadodsantos@yahoo.com>
2021-05-31 13:13:04 +0100

    Update Mocha translation: beforeafter.

--- 1-js/03-code-quality/05-testing-mocha/beforeafter.view/test.js
+++ 1-js/03-code-quality/05-testing-mocha/beforeafter.view/test.js
@@ -1,10 +1,16 @@
 describe("teste", function() {
 
-  before(() => alert("Testes iniciados – antes de todos os testes"));
-  after(() => alert("Testes terminados – depois de todos os testes"));
+  // Mocha geralmente espera pelos testes por 2 segundos, antes de os considerar errados
+  
+  this.timeout(200000); // Com este código nós aumentamos isso - neste caso para 200,000 milisegundos (≈ 3.333 minutos)
 
-  beforeEach(() => alert("antes de um teste – entrando para um teste"));
-  afterEach(() => alert("depois de um teste – saindo de um teste"));
+  // Isto, por causa da função "alert", porque se você se demorar a pressionar o botão "OK", os testes não conseguirão passar um possível teste de validade!
+  
+  before(() => alert("Testes começam – antes de todos os testes"));
+  after(() => alert("Testes terminam – depois de todos os testes"));
+
+  beforeEach(() => alert("Antes de um teste – entrando para um teste"));
+  afterEach(() => alert("Depois de um teste – saindo de um teste"));
 
   it('teste 1', () => alert(1));
   it('teste 2', () => alert(2));
