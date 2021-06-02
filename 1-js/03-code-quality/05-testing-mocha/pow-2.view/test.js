85e5bd659feea46e2e81a24e454e580eab7d5a9b
Osvaldo Dias dos Santos <vadodsantos@yahoo.com>
2021-06-02 16:03:21 +0100

    Update Mocha translation: pow-2, pow-3.

--- 1-js/03-code-quality/05-testing-mocha/pow-2.view/test.js
+++ 1-js/03-code-quality/05-testing-mocha/pow-2.view/test.js
@@ -1,11 +1,11 @@
 describe("pow", function() {
 
-  it("2 elevado à potência 3 é 8", function() {
+  it("2 elevado à potência 3 são 8", function() {
     assert.equal(pow(2, 3), 8);
   });
 
-  it("3 elevado à potência 3 é 27", function() {
-    assert.equal(pow(3, 3), 27);
+  it("3 elevado à potência 4 são 81", function() {
+    assert.equal(pow(3, 4), 81);
   });
 
 });
