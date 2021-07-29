c86916ed5de0340904c002705cfc44a1206780ed
Nazaré da Piedade <31008635+nazarepiedady@users.noreply.github.com>
2021-07-29 20:06:21 +0100

    Update 1-js/06-advanced-functions/09-call-apply-decorators/01-spy-decorator/_js.view/solution.js
    Co-authored-by: Osvaldo Dias dos Santos <vadodsantos@yahoo.com>

--- 1-js/06-advanced-functions/09-call-apply-decorators/01-spy-decorator/_js.view/solution.js
+++ 1-js/06-advanced-functions/09-call-apply-decorators/01-spy-decorator/_js.view/solution.js
@@ -1,7 +1,7 @@
 function spy(func) {
 
   function wrapper(...args) {
-    // usando ...args ao invés de arguments para guardar o array "real" dentro de wrapper.calls
+    // usando ...args ao invés de arguments para guardar o "verdadeiro" array dentro de wrapper.calls
     wrapper.calls.push(args);
     return func.apply(this, args);
   }
